0bf99223c06e515660c5b151d048adeab7baf84d
Xing Zhou <xing@ohdyno.me>
2016-11-23 15:46:44 -0800

    BowlingGameKata with Kotlin

---
+++ src/BowlingGameTest.kt
@@ -0,0 +1,61 @@
+import org.testng.Assert.assertEquals
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+
+class BowlingGameTest {
+    private var game: Game? = null
+
+    @BeforeMethod
+    fun setUp() {
+        game = Game()
+    }
+
+    @Test fun testGutterGame() {
+        game?.rollMany(n = 20, pins = 0)
+        assertEquals(game?.score, 0)
+    }
+
+    @Test
+    fun testAllOnes() {
+        game?.rollMany(n = 20, pins = 1)
+        assertEquals(game?.score, 20)
+    }
+
+    @Test
+    fun testOneSpare() {
+        game?.rollSpare()
+        game?.roll(3)
+        game?.rollMany(n = 17, pins = 0)
+        assertEquals(game?.score, 16)
+    }
+
+    @Test
+    fun testOneStrike() {
+        game?.rollStrike()
+        game?.roll(3)
+        game?.roll(4)
+        game?.rollMany(16, 0)
+        assertEquals(game?.score, 24)
+    }
+
+    @Test
+    fun testPerfectGame() {
+        game?.rollMany(n = 12, pins = 10)
+        assertEquals(game?.score, 300)
+    }
+
+    private fun Game.rollMany(n: Int, pins: Int) {
+        (1..n).forEach { this.roll(pins) }
+    }
+
+    private fun Game.rollSpare() {
+        this.roll(5)
+        this.roll(5)
+    }
+
+    private fun Game.rollStrike() {
+        this.roll(10)
+    }
+
+}
+
---
+++ src/Game.kt
@@ -0,0 +1,46 @@
+class Game {
+    var rolls = IntArray(21) {0}
+    var currentRoll = 0
+
+    var score = 0
+        get() {
+            var score = 0
+            var frameIndex = 0
+            (1..10).forEach {
+                when {
+                    isSpare(frameIndex) -> {
+                        score += scoreSpare(frameIndex)
+                        frameIndex += 2
+                    }
+                    isStrike(frameIndex) -> {
+                        score += scoreStrike(frameIndex)
+                        frameIndex++
+                    }
+                    else -> {
+                        score += scoreFrame(frameIndex)
+                        frameIndex += 2
+                    }
+                }
+            }
+
+            return score
+        }
+
+    fun roll(pins: Int) {
+        rolls[currentRoll++] = pins
+    }
+
+    private fun scoreStrike(frameIndex: Int) = 10 + strikeBonus(frameIndex)
+
+    private fun strikeBonus(frameIndex: Int) = rolls[frameIndex + 1] + rolls[frameIndex + 2]
+
+    private fun scoreSpare(frameIndex: Int) = scoreFrame(frameIndex) + spareBonus(frameIndex)
+
+    private fun  spareBonus(frameIndex: Int) = rolls[frameIndex + 2]
+
+    private fun isStrike(frameIndex: Int) = rolls[frameIndex] == 10
+
+    private fun isSpare(frameIndex: Int) = scoreFrame(frameIndex) == 10
+
+    private fun scoreFrame(frameIndex: Int) = rolls[frameIndex] + rolls[frameIndex + 1]
+}
